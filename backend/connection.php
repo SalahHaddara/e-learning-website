11cedd3aa054774ad1c89c6b98601cf702ebdbfe
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-17 11:42:41 +0200

    backend: add connection.php

---
+++ backend/connection.php
@@ -0,0 +1,11 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "e-learning-website";
+
+$conn = new mysqli($server, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
