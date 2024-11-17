b115ffd1059e1b17043dcf5618370bbb3fff1687
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-17 11:42:21 +0200

    frontend: add login file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,48 @@
+import axios from 'axios';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // const response = await axios.post('/backend/login', formData);
+        // console.log(response.data);
+        console.log(formData);
+        navigate('/dashboard');
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        if (name === "email") {
+            setFormData({...formData, email: value});
+        } else if (name === "password") {
+            setFormData({...formData, password: value});
+        }
+    };
+
     return (
         <div>
             <h2>Login</h2>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="email"></label>
-                <input type="text" id="email" name="email" placeholder="email"/>
-                <label htmlFor="pasword"></label>
-                <input type="pasword" id="pasword" name="pasword" placeholder="pasword"/>
+                <input type="email" id="email" name="email" placeholder="email" value={formData.email}
+                       onChange={handleChange}/>
+                <label htmlFor="password"></label>
+                <input type="password" id="password" name="password" placeholder="password" value={formData.password}
+                       onChange={handleChange}/>
 
-                <button type="submit">Register</button>
+                <button type="submit">Login</button>
             </form>
-
-        </div>);
+        </div>
+    );
 };
 
 export default Login;
