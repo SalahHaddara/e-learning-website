2a052113a7197eb59d4adb1a6bd67a9b72dce347
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 07:21:38 +0200

    Front-end: add protectedRoute cases

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,27 @@
 import {Navigate} from 'react-router-dom';
-import {isAdmin} from '../services/auth';
 
-const ProtectedRoute = ({children}) => {
-    if (!localStorage.token || !isAdmin()) {
+const ProtectedRoute = ({children, allowedRole}) => {
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!token || !user) {
         return <Navigate to="/login"/>;
     }
+
+    if (user.role !== allowedRole) {
+
+        switch (user.role) {
+            case 'admin':
+                return <Navigate to="/dashboard"/>;
+            case 'instructor':
+                return <Navigate to="/instructor"/>;
+            case 'student':
+                return <Navigate to="/student"/>;
+            default:
+                return <Navigate to="/login"/>;
+        }
+    }
+
     return children;
 };
 
