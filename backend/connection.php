41f574b0471f5e67762f5de65bd1742a5afdaa64
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 05:17:36 +0200

    backend: fix course

--- backend/connection.php
+++ backend/connection.php
@@ -1,8 +1,13 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $server = "localhost";
 $user = "root";
