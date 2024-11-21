720d3aca59b279f6596c4012135619ed5db88b81
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 02:59:56 +0200

    Front-end: add user card

---
+++ src/pages/dashboard/components/UserCard.jsx
@@ -0,0 +1,19 @@
+import './Dashboard.css';
+
+const UserCard = ({user, onBan}) => {
+    return (
+        <div className="card">
+            <h3>{user.username}</h3>
+            <p>Email: {user.email}</p>
+            <p>Role: {user.role}</p>
+            <p>Status: {user.status ? 'Active' : 'Banned'}</p>
+            {user.status && (
+                <button className="button delete" onClick={() => onBan(user.id)}>
+                    Ban User
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default UserCard;
