c4a2864d3847afa3c2e038ac5b0736768d75fd45
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 07:07:40 +0200

    Front-end: add student and instructor pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login.jsx";
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import InstructorPage from "./pages/intructorPage/IntructorPage.jsx";
+import StudentPage from "./pages/studentPage/StudentsPage.jsx";
 
 function App() {
 
@@ -15,10 +17,21 @@ function App() {
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/dashboard" element={
-                        <ProtectedRoute>
+                        <ProtectedRoute allowedRole="admin">
                             <Dashboard/>
                         </ProtectedRoute>
-                    }/> </Routes>
+                    }/>
+                    <Route path="/instructor" element={
+                        <ProtectedRoute allowedRole="instructor">
+                            <InstructorPage/>
+                        </ProtectedRoute>
+                    }/>
+                    <Route path="/student " element={
+                        <ProtectedRoute allowedRole="student">
+                            <StudentPage/>
+                        </ProtectedRoute>
+                    }/>
+                </Routes>
             </BrowserRouter>
         </>
     )
