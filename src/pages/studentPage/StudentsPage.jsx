9cd338e2052e90be010d62715228f8a7ecfcbdb6
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 07:02:59 +0200

    Front-end: add StudentsPage.jsx

---
+++ src/pages/studentPage/StudentsPage.jsx
@@ -0,0 +1,8 @@
+const StudentPage = () => {
+    return (
+        <div className="page">
+            <h1>Student Dashboard</h1>
+            <p>Welcome, Student! Your enrolled courses will appear here.</p>
+        </div>
+    );
+};
