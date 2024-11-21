c05593b5fd87a7272a0ed17af9747abc79fc678f
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 06:58:33 +0200

    Front-end: add instructor page

---
+++ src/pages/intructorPage/IntructorPage.jsx
@@ -0,0 +1,10 @@
+const InstructorPage = () => {
+    return (
+        <div className="page">
+            <h1>Instructor Dashboard</h1>
+            <p>Welcome, Instructor! Your assigned courses will appear here.</p>
+        </div>
+    );
+};
+
+export default InstructorPage;
