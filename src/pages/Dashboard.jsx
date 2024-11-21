eb83c462b036a1e36122ebc3d96e9976ee30f93a
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 03:40:49 +0200

    Front-end: add useStates in Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    const [users, setUsers] = useState([]);
+    const [courses, setCourses] = useState([]);
+    const [showCreateCourse, setShowCreateCourse] = useState(false);
+    const [instructors, setInstructors] = useState([]);
+
+
+};
+
+export default Dashboard;
