d70c7234dbe7fe06273d59c8bee72232fea69703
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-21 05:45:25 +0200

    backend: fix ban

--- src/pages/dashboard/components/UserCard.jsx
+++ src/pages/dashboard/components/UserCard.jsx
@@ -6,8 +6,8 @@ const UserCard = ({user, onBan}) => {
             <h3>{user.username}</h3>
             <p>Email: {user.email}</p>
             <p>Role: {user.role}</p>
-            <p>Status: {user.status ? 'Active' : 'Banned'}</p>
-            {user.status && (
+            <p>Status: {user.status === "1" ? 'Active' : 'Banned'}</p>
+            {user.status === "1" && (
                 <button className="button delete" onClick={() => onBan(user.id)}>
                     Ban User
                 </button>
